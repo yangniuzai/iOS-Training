2809096e46381935374ac5178216464e042dff70
yangniuzai <yangniuzai@gmail.com>
2017-11-29 00:08:33 +0100

    chekmark added

--- Checklists/Checklists/ChecklistViewController.swift
+++ Checklists/Checklists/ChecklistViewController.swift
@@ -20,12 +20,41 @@ class ChecklistViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return 100
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let label = cell.viewWithTag(1000) as! UILabel
+        
+        if indexPath.row%5 == 0 {
+            label.text = "Walk the cat"
+        } else if indexPath.row%5 == 1 {
+            label.text = "Brush my teeth"
+        } else if indexPath.row%5 == 2{
+            label.text = "Learn iOS development"
+        } else if indexPath.row%5 == 3 {
+            label.text = "Sccoer Practice"
+        } else if indexPath.row%5 == 4 {
+            label.text = "Eat ice cream"
+        }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .none {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
